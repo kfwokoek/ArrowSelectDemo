84a01e84285cbfdd93ac28a3088d041a3f770e72
kurt wokoek <kur_wokoek1@baylor.edu>
2024-08-21 14:09:47 -0500

    right arrow button implementation

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,17 @@
 import {View, Text, SafeAreaView} from 'react-native'
 import { useState } from 'react'
-import Icons from '../constants/Icons'
 import { ViewStyles } from '../styles/ViewStyles'
 import { TextStyles } from '../styles/TextStyles'
+import RightArrowButton from '../components/buttons/RightArrowButton'
 
 const index = () => {
+
+    const [item, setItem] = useState(0)
     
     return (
         <SafeAreaView style={ViewStyles.safeAreaStyle}>
             <Text style={TextStyles.primaryText}>App</Text>
+            <RightArrowButton numItems={4} state={item} setState={setItem} width={50} height={50}/>
         </SafeAreaView>
     )
 }
--- components/buttons/RightArrowButton.jsx
+++ components/buttons/RightArrowButton.jsx
@@ -1,16 +1,22 @@
 import { TouchableOpacity, Image } from "react-native"
+import Icons from '../../constants/Icons'
 
-const RightArrowButton = ({ numItems, state, setState }) => {
+
+const RightArrowButton = ({ numItems, state, setState, width, height }) => {
     
     const handlePress = () => {
         if (state < numItems - 1) {
-            setState((state + 1) % numItems)
+            setState(state + 1)
         }
+        else {
+            setState(0)
+        }
+        console.log(state)
     }
 
     return (
         <TouchableOpacity onPress={handlePress}>
-            <Image source={Icons.rightArrow} style={IconStyles.iconStyle} />
+            <Image source={Icons.right_arrow} style={{ resizeMode:"cover", width: width, height: height }}/>
         </TouchableOpacity>
     )
 }
